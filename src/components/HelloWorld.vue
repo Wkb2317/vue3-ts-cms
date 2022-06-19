ed8da1f35ef89405a173083d82d7ba65abf6b5bc
Wkb2317 <2576267399@qq.com>
2022-06-19 23:17:28 +0800

    feat: husky test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+    
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -95,14 +108,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String,
-  },
-});
+    msg: String
+  }
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
